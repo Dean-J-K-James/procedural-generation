2ae2afe644b2778a93a499ea01fe1b5d87d9bdc2
Dean-J-K-James <60291976+Dean-J-K-James@users.noreply.github.com>
2020-11-07 21:18:12 +0000

    Added random walk cave generation

---
+++ Procedural Cave Generation with Random Walk/README.md
@@ -0,0 +1,5 @@
+# Procedural Cave Generation with Random Walk
+
+Download this folder and open the HTML file to view the page.
+
+Styles and scripts are seperated into CSS and JS files.
---
+++ Procedural Cave Generation with Random Walk/random_walk.js
@@ -0,0 +1,59 @@
+
+// Gets the canvas from the HTML document.
+var canvas = document.getElementById("canvas").getContext("2d");
+
+// Initialises the map width and height.
+let map_size_x = 64;
+let map_size_y = 64;
+
+// A two dimensional array of coordinates.
+var cells = [];
+
+// Initialises the cells array so that it can work in two dimensions. Sets each cell to its default wall state.
+for (let x = 0; x < map_size_x; x++) {
+    cells[x] = [];
+    for (let y = 0; y < map_size_y; y++) {
+        cells[x][y] = 0;
+    }
+}
+
+// Manages the flow of the program. First creates the cave, then renders it.
+create_cave();
+render_cave();
+
+// Creates the cave by running the random walk algorithm.
+function create_cave() {
+
+    var x = map_size_x / 2;
+    var y = map_size_y / 2;
+
+    for (let i = 0; i <= 2000; i++) {
+
+        cells[x][y] = 1;
+
+        switch (Math.floor(Math.random() * 4)) {
+            case 0: { y++; break; }
+            case 1: { x++; break; }
+            case 2: { y--; break; }
+            case 3: { x--; break; }
+        }
+
+        x = Math.max(1, Math.min(x, map_size_x - 2));
+        y = Math.max(1, Math.min(y, map_size_y - 2));
+    }
+}
+
+// Renders the cave by looping through each cell and drawing a square.
+function render_cave() {
+
+    for (let x = 0; x < map_size_x; x++)
+        for (let y = 0; y < map_size_y; y++) {
+
+            switch (cells[x][y]) {
+                case 0: { canvas.fillStyle = "rgb(066, 071, 076)"; break; }
+                case 1: { canvas.fillStyle = "rgb(251, 116, 106)"; break; }
+            }
+
+            canvas.fillRect(x * 8, y * 8, 8, 8);
+        }
+}
